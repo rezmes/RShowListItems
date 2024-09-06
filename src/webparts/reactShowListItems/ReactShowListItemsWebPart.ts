0726aea757561bcf53f39f80f505318b11aa7d5e
rezmes <rezmes@gmail.com>
2024-09-06 16:20:00 +0330

    start - install pnp@2.06 - JQuery@2 - JQueryui

---
+++ src/webparts/reactShowListItems/ReactShowListItemsWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'ReactShowListItemsWebPartStrings';
+import ReactShowListItems from './components/ReactShowListItems';
+import { IReactShowListItemsProps } from './components/IReactShowListItemsProps';
+
+export interface IReactShowListItemsWebPartProps {
+  description: string;
+}
+
+export default class ReactShowListItemsWebPart extends BaseClientSideWebPart<IReactShowListItemsWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IReactShowListItemsProps > = React.createElement(
+      ReactShowListItems,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/reactShowListItems/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IReactShowListItemsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ReactShowListItemsWebPartStrings' {
+  const strings: IReactShowListItemsWebPartStrings;
+  export = strings;
+}
